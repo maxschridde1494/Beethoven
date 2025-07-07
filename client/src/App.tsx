562e0fb33633e68677c8622f47b5688c8b474ae4
Max Schridde <max@roboflow.com>
2025-07-06 19:36:44 -0700

    update README with demo gif

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,24 @@
 import './App.css'
+import ColorKey from './components/ColorKey'
 import { LiveRtspPlayer } from './components/LiveRtspPlayer'
-import { useRealTime } from './hooks/useRealTime';
-import RelativePositions from './components/RelativePositions';
-import type { Detection } from './types';
+import { CLASS_COLORS } from './utils/colorUtils'
+// import { useRealTime } from './hooks/useRealTime';
+// import RelativePositions from './components/RelativePositions';
+// import type { Detection } from './types';
 
 function App() {
-  const { relativePositions, isLoading } = useRealTime('ws://localhost:8000/ws');
+  // const { relativePositions, isLoading } = useRealTime('ws://localhost:8000/ws');
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
   
   return (
     <div>
-      <h1>Beethoven</h1>
-      <div>
-        <RelativePositions positions={relativePositions as unknown as { [key: string]: Detection[] }} />
+      <h1 style={{ margin: 0, padding: 0 }}>Beethoven</h1>
+      <div style={{ paddingBottom: '10px', width: '20%' }}>
+        {/* <RelativePositions positions={relativePositions as unknown as { [key: string]: Detection[] }} /> */}
+        <ColorKey colorMap={CLASS_COLORS} />
       </div>
       <div style={{ display: 'flex', flexDirection: 'row', gap: '10px' }}>
         <LiveRtspPlayer
